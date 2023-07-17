47c0c5504f58ab0657d77967338668547c32dc04
Challihj <challihj@dukes.jmu.edu>
2023-07-17 08:14:30 -0400

    cleaned up code a little bit, still need 12 and 24 hr functionality

--- Clock/src/clock/Clock.java
+++ Clock/src/clock/Clock.java
@@ -6,7 +6,6 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -25,12 +24,12 @@ public class Clock extends Application {
 
     Text time = new Text();
     // Add functionality for switching between 12 and 24 hr format
-    Button btn24 = new Button("24 Hour Clock");
-    Button btn12 = new Button("12 Hour Clock");
+    Button btn24 = new Button("24 Hour Format");
+    Button btn12 = new Button("12 Hour Format");
 
     @Override
     public void start(Stage stage) {
-        Scene scene = new Scene(clock(), 640, 300);
+        Scene scene = new Scene(GridPane(), 640, 300);
         scene.setFill(Color.WHITE);
         stage.setMaxHeight(300);
         stage.setMaxWidth(640);
@@ -40,8 +39,11 @@ public class Clock extends Application {
         stage.setTitle("Digital Clock");
         stage.show();
     }
-
-    private GridPane clock() {
+public void clearGrid(GridPane grid) {
+        grid.getChildren().clear();
+    }
+    
+    public GridPane GridPane() {
         HBox hbox = new HBox();
         setTime();
         GridPane grid = new GridPane();
@@ -50,9 +52,11 @@ public class Clock extends Application {
         hbox.getChildren().addAll(btn12, btn24);
         hbox.setSpacing(400);
         grid.setAlignment(Pos.CENTER);
-
+        
         return grid;
+
     }
+    
 
     private void setTime() {
         getTime();
@@ -62,6 +66,7 @@ public class Clock extends Application {
     }
 
     private void getTime() {
+        
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0), new EventHandler<ActionEvent>() {
             @Override
